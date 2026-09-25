e4ec3e5ad750e0994ebe8894f9580dc3aa53a413
David De La Toba <ddelatob@uci.edu>
2026-09-25 09:47:19 -0700

    Configured RCC, UART, GPIO partially

--- Inc/pins.h
+++ Inc/pins.h
@@ -1,6 +1,6 @@
 #ifndef _VVC_PINS_H
 #define _VVC_PINS_H
 
-#define BUTTON_PIN (2U)
-#define LED_PIN (1U)
+#define TX_PIN (2U)
+#define RX_PIN (1U)
 #endif
--- Src/main.c
+++ Src/main.c
@@ -4,5 +4,20 @@
 #include "pins.h"
 
 int main(void) {
+    RCC->APB2ENR |= (1UL << 14U);
+    RCC->AHB2ENR |= (1UL << 0U);
+
+    // 4E6 MHz / 9600 Baud rounded up
+    uint32_t usartDiv = 418UL;
+
+    USART1->BRR = usartDiv;
+    USART1->CR1 |= (1UL << 3U);
+    USART1->CR1 |= (1UL << 0U);
+
+    GPIOA->MODER &= ~(0x3UL << (TX_PIN * 2U));
+    GPIOA->MODER |= (0x2UL << (TX_PIN * 2U));
+
+    GPIOA->MODER &= ~(0x3UL << (RX_PIN * 2U));
+    GPIOA->MODER |= (0x2UL << (RX_PIN * 2U));
 
 }
